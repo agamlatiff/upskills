da3a3198e45cbf00a41a24c87b23388cd9e56eb3
agamlatiff <agam.latiff@gmail.com>
2025-10-28 06:44:21 +0700

    Routing project

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,43 @@
 <?php
 
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [FrontController::class, "index"])->name("front.index");
+Route::get("/pricing", [FrontController::class, "pricing"])->name("front.pricing");
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::match(["get", "post"], "/booking/payment/midtrans/notification", [FrontController::class, "paymentMidtransNotification"])->name("front.payment_midtrans_notification");
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware("role.student")->group(function () {
+        Route::get("/dashboard/subscriptions/", [DashboardController::class, "subscriptions"])->name("dashboard.subscriptions");
+        Route::get("/dashboard/subscription/{transaction}", [DashboardController::class, "subscription_details"])->name("dashboard.subscription_details");
+
+        Route::get("/dashboard/courses/{course:slug}", [CourseController::class, "index"])->name("dashboard");
+        Route::get("/dashboard/search/courses", [CourseController::class, "search_courses"])->name("dashboard.search_courses");
+
+        Route::get("/dashboard/search/courses", [CourseController::class, "search_courses"])->name("dashboard_search_courses");
+
+        Route::middleware(["check.subscription"])->group(function () {
+            Route::get("/dashboard/join/{course:slug}", [CourseController::class, "join"])->name("dashboard.course.join");
+
+            Route::get("/dashboard/learning/{course:slug}/{courseSection}/{sectionContent}", [CourseController::class, "learning"])->name("dashboard.course.learning");
+
+            Route::get("/dashboard/learning/{course:slug}/finished", [CourseController::class, "learning_finished"])->name("dashboard.course.leaning.finished");
+        });
+        
+        Route::get("/checkout/success", [FrontController::class, "checkout_success"])->name("front.checkout.success");
+        Route::get("/checkout/{pricing}", [FrontController::class, "checkout"])->name("front.checkout");
+        
+        Route::post("/booking/payment/midtrans", [FrontController::class, "paymentStoreMidtrans"])->name("front.payment_store_midtrans");
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';

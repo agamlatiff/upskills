c051c45fd89b1fe14b09473c20b7ef40b5b8b0f9
agamlatiff <agam.latiff@gmail.com>
2025-10-27 17:41:33 +0700

    CRUD data course mentor

--- app/Filament/Resources/CourseMentorResource.php
+++ app/Filament/Resources/CourseMentorResource.php
@@ -5,10 +5,15 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\CourseMentorResource\Pages;
 use App\Filament\Resources\CourseMentorResource\RelationManagers;
 use App\Models\CourseMentor;
+use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -23,7 +28,15 @@ class CourseMentorResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Select::make("course_id")->relationship("course", "name")->searchable()->preload()->required(),
+                Select::make("user_id")->label("Mentor")->options(function () {
+                    return User::role("mentor")->pluck("name", "id");
+                })->searchable()->preload()->required(),
+                Textarea::make("about")->required(),
+                Select::make("is_active")->options([
+                    true => "Active",
+                    false => "Banned"
+                ])->required()
             ]);
     }
 
@@ -31,7 +44,10 @@ class CourseMentorResource extends Resource
     {
         return $table
             ->columns([
-                //
+                ImageColumn::make("mentor.photo"),
+                TextColumn::make("mentor.name")->sortable()->searchable(),
+                ImageColumn::make("course.thumbnail")->sortable()->searchable(),
+                TextColumn::make("course.name")->sortable()->searchable(),
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
